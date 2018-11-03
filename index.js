06e60eb189b9b6bbc70f413c4097806fea2d1b96
Kedar Bell <kbell99@outlook.com>
2018-11-03 16:35:36 -0500

    ugh

--- index.js
+++ index.js
@@ -10,49 +10,54 @@ $( "#searchButton" ).click(function( event ) {
     var end = encodeURIComponent($("#end").val());
 
   
-  $.getJSON( "./api/validate.php/?query="+start, function( data ) {
+  $.getJSON( "./api/validate.php/?query="+start, ( data ) => {
+   
+  }).done(function(){
     startCoords = data.coords;
+    $.getJSON( "./api/validate.php/?query="+end, ( data ) => {
+      endCoords = data.coords;
+    }).done(function(){
+      
+    });
+    $.getJSON( "./api/index.php?start="+startCoords+"&end="+endCoords, function( data ) {
+      // Put the results in a div
+      
+        let content = ""
+        alert("test");
+        console.log(data);
+        for (var key in data) {
+            content += "<tr>"
+            if (json.hasOwnProperty(key)) {
+               let name = data[key].name;
+               let currency = data[key].currency;
+               let price = data[key].price;
+               let distance = data[key].distance;
+               let time = data[key].time;
+               let timeInMin = Number(time)/60;
+               
+               if(!currency){
+    
+               }
+               else{
+                    // $("<td id = '" +name+"'>"+name+"</td>").appendTo("#result");
+                    // $("<td id = '" +price+"'><p id='"+currency +"'>"+price+"</td>").appendTo("#result");
+                    // $("<td>"+distance+" miles</td>").appendTo("#result");
+                    // $("<td>"+timeInMin+" minutes</td>").appendTo("#result");
+                    content += "<td id = '" +name+"'>"+name+"</td>";
+                    content += "<td id = '" +price+"'><p id='"+currency +"'>"+price+"</td>";
+                    content += "<td>"+distance+" miles</td>"
+                    content += "<td>"+timeInMin+" minutes</td>"
+                }
+            }
+            content += "</tr>"
+         }
+         $("#result").empty().append(content);
+      });
   });
 
-  $.getJSON( "./api/validate.php/?query="+end, function( data ) {
-    endCoords = data.coords;
-  });
-
+ 
     // Send the data using post
 
  
-  $.getJSON( "./api/index.php?start="+startCoords+"&end="+endCoords, function( data ) {
-  // Put the results in a div
   
-    let content = ""
-    alert("test");
-    console.log(data);
-    for (var key in data) {
-        content += "<tr>"
-        if (json.hasOwnProperty(key)) {
-           let name = data[key].name;
-           let currency = data[key].currency;
-           let price = data[key].price;
-           let distance = data[key].distance;
-           let time = data[key].time;
-           let timeInMin = Number(time)/60;
-           
-           if(!currency){
-
-           }
-           else{
-                // $("<td id = '" +name+"'>"+name+"</td>").appendTo("#result");
-                // $("<td id = '" +price+"'><p id='"+currency +"'>"+price+"</td>").appendTo("#result");
-                // $("<td>"+distance+" miles</td>").appendTo("#result");
-                // $("<td>"+timeInMin+" minutes</td>").appendTo("#result");
-                content += "<td id = '" +name+"'>"+name+"</td>";
-                content += "<td id = '" +price+"'><p id='"+currency +"'>"+price+"</td>";
-                content += "<td>"+distance+" miles</td>"
-                content += "<td>"+timeInMin+" minutes</td>"
-            }
-        }
-        content += "</tr>"
-     }
-     $("#result").empty().append(content);
-  });
 });
