59ac88fa3ae5dba2989577fc4e6186e90139b349
Jack Biggin <me@jackbiggin.me>
2018-11-03 10:04:50 -0400

    Support for Bird
    Using a totally official API

--- api/apis/bird.php
+++ api/apis/bird.php
@@ -1,24 +1,51 @@
 <?php
-
-$startPoint = htmlspecialchars($_GET['start']);
-$endPoint = htmlspecialchars($_GET['end']);
-$deviceID = 'f0022387-1e81-4763-88db-ccdb894d8f86';
-$ch = curl_init();
 $birdStartPoint = explode(',', $startPoint);
 $birdEndPoint = explode(',', $endPoint);
+
+//Generates bird API access token
+$ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "https://api.bird.co/user/login");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch,CURLOPT_HTTPHEADER, array('Device-id: '.$deviceID, 'Platform: ios', 'Content-type: application/json'));
+curl_setopt($ch,CURLOPT_HTTPHEADER, array('Device-id: '.$config['BIRD_PRICING']['birdGUID'], 'Platform: ios', 'Content-type: application/json'));
 curl_setopt($ch, CURLOPT_POSTFIELDS, '{"email": "'.time().'@example.com"}');
+//Currently generates new "email" for each request - should change to keep key in session and only refetch when expires
 $birdResults = curl_exec($ch);
 curl_close($ch);
-
 $birdResultsAccessID = json_decode($birdResults, true)['token'];
-// $birdResultsAccessID = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJBVVRIIiwidXNlcl9pZCI6ImFiMjRhYzI0LWEyYzItNDNiNy05NDliLWU0YzRiNmQ3M2IzOSIsImRldmljZV9pZCI6ImYwMDIyMzg3LTFlODEtNDc2My04OGRiLWNjZGI4OTRkOGY4NiIsImV4cCI6MTU3Mjc2MzcxMX0.6HM1Mo5PMneztiqUYJLLUymRiS9T1KRoml7XmawOUEs';
+
+//Uses token to find locations of nearby birds
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "https://api.bird.co/bird/nearby?latitude=".$birdStartPoint[0]."&longitude=".$birdStartPoint[1]."&radius=1000");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch,CURLOPT_HTTPHEADER, array('Authorization: Bird '.$birdResultsAccessID, 'Device-id: '.$deviceID, 'App-Version: 3.0.5', 'Location: {"latitude":'.$birdStartPoint[0].',"longitude":'.$birdStartPoint[1].',"altitude":500,"accuracy":100,"speed":-1,"heading":-1}'));
-echo $birdResults = curl_exec($ch);
+curl_setopt($ch,CURLOPT_HTTPHEADER, array('Authorization: Bird '.$birdResultsAccessID, 'Device-id: '.$config['BIRD_PRICING']['birdGUID'], 'App-Version: 3.0.5', 'Location: {"latitude":'.$birdStartPoint[0].',"longitude":'.$birdStartPoint[1].',"altitude":500,"accuracy":100,"speed":-1,"heading":-1}'));
+$birdResults = curl_exec($ch);
 curl_close($ch);
+$birdResults = json_decode($birdResults, true);
+if (sizeof($birdResults['birds']) == 0) {
+  // echo "No birds avaliable";
+  exit;
+}
+$closestBirdLocation = $birdResults['birds']['0']['location'];
+
+//Calculates time and distance to walk to Bird
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "https://route.api.here.com/routing/7.2/calculateroute.json?app_id=".$config['HERE_APP_ID']."&app_code=".$config['HERE_APP_CODE']."&waypoint0=geo!" . $startPoint . "&waypoint1=" .$closestBirdLocation['latitude'] . ',' .$closestBirdLocation['longitude'] . "&mode=fastest;pedestrian");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$walkToBird = json_decode(curl_exec($ch), true);
+curl_close($ch);
+$birdWalkDistance = round(0.000621371 * $walkToBird['response']['route'][0]['summary']['distance'], 2);
+$birdWalkTime = $walkToBird['response']['route'][0]['summary']['baseTime'];
+
+//Calculates time, distance, and price to ride Bird to destination
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "https://route.api.here.com/routing/7.2/calculateroute.json?app_id=".$config['HERE_APP_ID']."&app_code=".$config['HERE_APP_CODE']."&waypoint0=geo!" . $closestBirdLocation['latitude']. ',' .$closestBirdLocation['longitude'] . "&waypoint1=" . $endPoint . "&mode=fastest;pedestrian");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$birdRideResult = json_decode(curl_exec($ch), true);
+curl_close($ch);
+$birdRideDistance = 0.000621371 * $birdRideResult['response']['route'][0]['summary']['distance'];
+$birdRideTime = ($birdRideDistance/$config['BIRD_PRICING']['birdSpeed'])*360;
+$birdPrice = (($config['BIRD_PRICING']['birdFlatFare'] + $birdRideTime/60)*$config['BIRD_PRICING']['birdPerMinuteCost']);
+
+//Final results push
+array_push($results, array('name' => 'Bird', 'distance' => round(($birdRideDistance + $birdWalkDistance), 2), 'currency' => 'USD', 'price' => round($birdPrice, 2), 'time' => round(($birdWalkTime + $birdRideTime), 2)));
