b14bd7ac9725a88eaec6342b3fdbf75c8c029772
Kedar Bell <kbell99@outlook.com>
2018-11-03 22:27:37 -0600

    hoping get works

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+window.onload = function() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const myParam = urlParams.get('myParam');
+  var isset = getParameterByName('go');
+  if(isset != ""){
+    endPlaceholder = document.getElementById('end');
+    endPlaceholder.setAttribute("placeholder", isset); 
+  }
+  return;
+};
+
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, '\\$&');
+  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
 let startCoords = "";
 let endCoords = "";
 
