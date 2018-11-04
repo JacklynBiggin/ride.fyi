aa7530f2293f19078f2e7f0a5c09d0fd5c4920ea
David Frankel <david.frankel@wustl.edu>
2018-11-03 20:08:06 -0500

    Fixed name analysis

--- api/apis/hybrid.php
+++ api/apis/hybrid.php
@@ -21,7 +21,7 @@ function getAllHybrids($startPoint, $endPoint, $pathOption) {
         $subResults = array_merge($subResults, getAllUbers($newStart, $newEnd));
         $subResults = array_filter($subResults); //Removes null results
         foreach ($subResults as $subResult) {
-          if (preg_match('/(Black)(Select)(Lux)/',$subResult)) {
+          if (preg_match('/(Black)(Select)(Lux)/',$subResult['name'])) {
             continue;
           }
           $newTransitOptions = getAllTransits($startPoint, $endPoint, 1, time()+$subResult['time']);
